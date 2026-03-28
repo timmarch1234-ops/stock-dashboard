489cf80b7786144da70414615d7e7e87f64f875a
James <james@Jamess-MacBook-Air.local>
2026-03-28 15:03:31 +1100

    feat: disable browser caching with no-store headers

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const path = require('path');
 
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.set('Pragma', 'no-cache');
+  res.set('Expires', '0');
+  next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 const ATH = {
